7daa52dc48bea59feabbdff7b151c74e35d05f71
Tsubasa <scientistjishan@gmail.com>
2023-03-06 00:08:27 +0530

    Add logged-in users as owners while creating cars

--- my-project/src/api/car/controllers/car.js
+++ my-project/src/api/car/controllers/car.js
@@ -6,4 +6,11 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::car.car');
+module.exports = createCoreController('api::car.car', ({ strapi }) => ({
+    async create(ctx) {
+        let { data } = ctx.request.body;
+        data["owner"] = ctx.state.user.id;
+        ctx.request.body = { data };
+        return await super.create(ctx);
+    },
+}));
